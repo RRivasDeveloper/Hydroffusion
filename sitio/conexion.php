0712ec6a93f5df4a5fe80cab400f0655ade913f5
sara123478 <92215171+sara123478@users.noreply.github.com>
2022-10-14 22:12:04 -0600

    Conexión a base de datos

---
+++ sitio/conexion.php
@@ -0,0 +1,9 @@
+﻿<?php
+	$mysqli=new mysqli("localhost","root","","hydro");
+	//$mysqli=new mysqli("SERVER","USER","PASS","BASE");
+	$mysqli->set_charset("utf8");
+	if(mysqli_connect_errno()){
+		echo 'Conexion Fallida : ', mysqli_connect_error();
+		exit();
+	}
+?>
